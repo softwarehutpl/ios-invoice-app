81a1f89fd7f7d2a0b6f20a6d95d301d59223407e
Damian <damian.szejnoch@gmail.com>
2019-09-06 15:56:07 +0200

    Added missing file

--- invoice-app/Modules/Client/EditClient/ViewModel/EditClientViewmodel.swift
+++ invoice-app/Modules/Client/EditClient/ViewModel/EditClientViewmodel.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+import UIKit
+
+class EditClientViewModel: EditClientViewModelType {
+    
+    //MARK: - Propierties
+    var client: ClientModel
+    
+
+    //MARK: - Private
+    private let sceneCoordinator: SceneCoordinatorType
+    private let clientStorageService: ClientStorageServiceType
+    
+    private var editedClientDetails: ClientDetailsModel?
+    private var editedClientAddress: ClientAddressModel?
+    
+    //MARK: - Inits
+    init(sceneCoordinator: SceneCoordinatorType, clientStorageService: ClientStorageServiceType,client: ClientModel) {
+        self.sceneCoordinator = sceneCoordinator
+        self.clientStorageService = clientStorageService
+        self.client = client
+    }
+}
+
+extension EditClientViewModel {
+    func fetchClientDetails() -> ClientDetailsModel {
+        let clientDetails = ClientDetailsModel(name: client.name, email: client.email, phone: client.phone)
+        return clientDetails
+    }
+    func fetchClientAddressDetails() -> ClientAddressModel {
+        let clientAddress = ClientAddressModel(address: client.address, postcode: client.postcode, city: client.city
+            , country: client.country)
+        return clientAddress
+    }
+    func getClientData() -> String {
+        return client.id
+    }
+    func editClient() {
+        clientStorageService.editClient(client: client)
+    }
+    func popToClientList(source: UIViewController) {
+        sceneCoordinator.pop(source: source, animated: true)
+    }
+    func getEditedClientDetails(clientDetails: ClientDetailsModel) {
+        self.client.name = clientDetails.name
+        self.client.email = clientDetails.email
+        self.client.phone = clientDetails.phone
+    }
+    func getEditedClientAddress(clientAddress: ClientAddressModel) {
+        self.client.address = clientAddress.address
+        self.client.postcode = clientAddress.postcode
+        self.client.city = clientAddress.city
+        self.client.country = clientAddress.country
+    }
+}
+
